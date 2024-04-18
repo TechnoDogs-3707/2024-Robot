5d89d48105eb875de7715c000f1b93c143f4426d
technodog <technodog3707@gmail.com>
2024-04-18 09:05:08 -0400

    Revert "Update at 'Wed Apr 17 19:09:01 EDT 2024'"
    This reverts commit aa924c2440e9e811aeae9ef5363928d49f5c3b34.

--- src/main/java/frc/robot/subsystems/intakeDeploy/IntakeDeploy.java
+++ src/main/java/frc/robot/subsystems/intakeDeploy/IntakeDeploy.java
@@ -18,7 +18,7 @@ public class IntakeDeploy extends SubsystemBase {
     public enum IntakePositionPreset {
         STOWED(0.3),
         EJECT(0.1875),
-        AMP_SCORE(0.586),
+        AMP_SCORE(0.48),
         AMP_SCORE_PRIME(0.49),
         AMP_ONE(0.44),
         HANDOFF(-0.03),
