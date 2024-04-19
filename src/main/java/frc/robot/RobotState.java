967983c0d413d288056ad0ec15fff9ae0b4861d2
technodog <technodog3707@gmail.com>
2024-04-19 16:04:36 -0400

    Update at 'Fri Apr 19 16:04:36 EDT 2024'

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -239,7 +239,7 @@ public class RobotState {
         GeomUtil.toTransform2d(
             AllianceFlipUtil.apply(
                 FieldConstants.ampCenter.plus(
-                    new Translation2d(-0.5, 0)
+                    new Translation2d(-0.5, 0.10)
                 )
             )
         );
@@ -255,7 +255,7 @@ public class RobotState {
         
         Rotation2d targetVehicleDirection = predictedVehicleFixedToTargetTranslation.getAngle();
         if (DriverStation.getAlliance().orElse(Alliance.Blue).equals(Alliance.Red)) {
-            targetVehicleDirection = targetVehicleDirection.rotateBy(Rotation2d.fromDegrees(-13));
+            targetVehicleDirection = targetVehicleDirection.minus(Rotation2d.fromDegrees(30));
         }
 
         latestMoonshotAngle = targetVehicleDirection;
